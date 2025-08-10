228f8f80f2aae5be46dc3dce5a56811aef8ca4cc
Jacob Harwood <Jsbro98@github.com>
2025-08-09 20:37:03 -0500

    Added gameLoop def

--- Main.lean
+++ Main.lean
@@ -8,6 +8,25 @@ open Std
 def min := 0
 def max := 100
 
+def gameLoop (solution : Nat) (guesses : Nat) : IO Unit := do
+  let stdIn ← IO.getStdin
+  let input ← stdIn.getLine
+
+  match input.toNat? with
+  | some guess =>
+    if guess > solution then
+      IO.println s!"Lower!"
+      gameLoop solution (guesses + 1)
+    else if guess < solution then
+      IO.println s!"Higher!"
+      gameLoop solution (guesses + 1)
+    else
+      IO.println s!"Congrats! You Win! Guesses: {guesses}"
+  | none =>
+    IO.println s!"Please enter a valid number"
+    gameLoop solution guesses
+
+
 def main : IO Unit :=
   do
     IO.println s!"Welcome to our guessing game"
