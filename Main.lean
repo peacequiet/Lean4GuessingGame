33d817d824e881938d3a170ebeace02d56edbda6
Jacob Harwood <Jsbro98@github.com>
2025-08-09 20:01:49 -0500

    added std import for later use. created vars for min & max. Edited the opening message

--- Main.lean
+++ Main.lean
@@ -1,10 +1,18 @@
 import Lean4GuessingGame
+import Std
+
+open Std
 
 #check IO.getStdin
 
+def min := 0
+def max := 100
+
 def main : IO Unit :=
   do
-    IO.println s!"What's your name?"
+    IO.println s!"Welcome to our guessing game"
+    IO.println s!"Guess the number between a range of values"
+
     let stdIn ← IO.getStdin
     let userInput ← stdIn.getLine
     IO.println s!"Hello, {userInput}!"
